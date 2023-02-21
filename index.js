644fcab11d0ee992fec654073ad577ed2919480c
JakeDengler <jakedengler25@gmail.com>
2023-02-21 17:39:49 -0500

    made it possible to play a round

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const playerSelection = prompt("What Pokemon Type will you choose; Fire, Water, or Grass? ")
-console.log("You have choosen " + playerSelection +" as your type")
+
+function getplayerChoice(){
+    const playerSelection = prompt("What Pokemon Type will you choose; Fire, Water, or Grass? ")
+    console.log("You have choosen " + playerSelection +" as your type")
+    return playerSelection
+}
 
 function getComputerChoice() {
-    const computerchoice = ['Fire', 'Water', 'Grass' ];
+    const computerchoice = ['fire', 'water', 'grass' ];
     const type = computerchoice[Math.floor(Math.random()* computerchoice.length)]
     return type
 }
 
+function playRound (){
+    playerSelection.toLowerCase();
+    if (playerSelection == 'fire') {
+        if (computerSelection == "fire") {
+            return console.log("Fire & Fire: Draw")
+        } else if (computerSelection == "water") {
+            return console.log("Fire & Water: Computer Wins")
+        } else {
+            return console.log("Fire & Grass: Player")
+        }
+
+    } else if (playerSelection == 'water') {
+        if (computerSelection == "water") {
+            return console.log("Water & Water: Draw")
+        } else if (computerSelection == "fire") {
+            return console.log("Water & Fire: Player Wins")
+        } else {
+            return console.log("Water & Grass: Computer Wins")
+        }
+    } else if (playerSelection == 'grass') {
+        if (computerSelection == "grass") {
+            return console.log("Grass & Grass: Draw")
+        } else if (computerSelection == "water") {
+            return console.log("Grass & Water: Player Wins")
+        } else {
+            return console.log("Grass & Fire: Comptuer Wins")
+        }
+    } else {
+        return console.log("Please refresh and enter a correct value")
+    }
+
+const playerSelection = getplayerChoice();
 const computerSelection = getComputerChoice();
+console.log("The computer has choosen " + computerSelection)
 console.log(playerSelection, computerSelection)
